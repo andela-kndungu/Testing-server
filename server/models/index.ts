0c3c36fc702473a9fc8f3459a629e767614f24a0
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-12-05 16:05:21 +0300

    Convert Sequelize code to Typescript

--- server/models/index.ts
+++ server/models/index.ts
@@ -5,8 +5,11 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require(__dirname + '/../config/config.json')[env];
-var db        = {};
+var config    = require('../config/config.js')[env];
+var db        = {
+  sequelize: '',
+  Sequelize: ''
+};
 
 if (config.use_env_variable) {
   var sequelize = new Sequelize(process.env[config.use_env_variable]);
