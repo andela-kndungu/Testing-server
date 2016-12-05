94ed58cd984006ff46e06bcbcbebae66e0114bee
Kinuthia Ndung'u <kinuthia.ndungu@andela.com>
2016-12-05 16:47:10 +0300

    Debug Heroku Postgres

--- server/models/index.ts
+++ server/models/index.ts
@@ -5,27 +5,28 @@ var path      = require('path');
 var Sequelize = require('sequelize');
 var basename  = path.basename(module.filename);
 var env       = process.env.NODE_ENV || 'development';
-var config    = require('../config/config.js')[env];
 var db        = {
   sequelize: '',
   Sequelize: ''
 };
 
 if (process.env['DATABASE_URL']) {
+  console.log(process.env['DATABASE_URL']);
   var sequelize = new Sequelize(process.env['DATABASE_URL']);
 } else {
+  var config    = require('../config/config.js')[env];
   var sequelize = new Sequelize(config.database, config.username, config.password, config);
 }
 
 fs
-  .readdirSync(__dirname)
-  .filter(function(file) {
-    return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
-  })
-  .forEach(function(file) {
-    var model = sequelize['import'](path.join(__dirname, file));
-    db[model.name] = model;
-  });
+.readdirSync(__dirname)
+.filter(function(file) {
+  return (file.indexOf('.') !== 0) && (file !== basename) && (file.slice(-3) === '.js');
+})
+.forEach(function(file) {
+  var model = sequelize['import'](path.join(__dirname, file));
+  db[model.name] = model;
+});
 
 Object.keys(db).forEach(function(modelName) {
   if (db[modelName].associate) {
